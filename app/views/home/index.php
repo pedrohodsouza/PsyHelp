d65c42f6428c6f37366f2fdec24e06548a7239ea
Pedro de Souza <pedrohodesouza@gmail.com>
2024-08-26 16:07:36 -0300

    Parte Inicial da Página Principal

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,10 +1,34 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
-    <title>Hello World</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    <h1>Hello World</h1>
-    <a>Teste</a>
+    <div class="header">
+        <img src="images/menu-icon.png" alt="Menu" class="icon">
+        <img src="images/psyhelp.png" alt="Logotipo" class="logo">
+        <img src="images/messages-icon.png" alt="Mensagens" class="messages-icon">
+    </div>
+    <div class="options-container">
+        <div class="option">
+            <img src="images/profissionais.png" alt="Profissionais" class="option-icon">
+            <span>Profissionais</span>
+        </div>
+        <div class="option">
+            <img src="images/dicas.png" alt="Dicas de Saúde" class="option-icon">
+            <span>Dicas de Saúde</span>
+        </div>
+        <div class="option">
+            <img src="images/receitas.png" alt="Receitas Farmacêuticas" class="option-icon">
+            <span>Receitas Farmacêuticas</span>
+        </div>
+        <div class="option">
+            <img src="images/agendamento.png" alt="Agendamento de Sessões" class="option-icon">
+            <span>Agendamento de Sessões</span>
+        </div>
+    </div>
 </body>
 </html>
